302b17df653db4d77febc2a6e06aa6f42a24798b
John Doe <betiaman@example.com>
2023-08-14 15:33:14 -0500

    scanner properly closed

--- GLab_ReadingFromConsole/ReadingStringFromConsole.java
+++ GLab_ReadingFromConsole/ReadingStringFromConsole.java
@@ -13,10 +13,12 @@ public class ReadingStringFromConsole {
         // System.out.println("s2 is " + s2);
         // System.out.println("s3 is " + s3);
 
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter a character: ");
-    String s = input.nextLine();
-    char ch = s.charAt(0);
-    System.out.println("The character entered is " + ch);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+        input.close();
     }
 }
